14d47a8b9b61d5f12e9adb8ee7b391db3b6f8523
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2020-02-28 16:11:48 +0000

    Spring initialize

---
+++ src/test/java/com/kaznowski/versionedloadbalancer/VersionedLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaznowski.versionedloadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionedLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
