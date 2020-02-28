01866963b91cb5aaa980a9fb0723edb690913fc2
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2020-02-28 16:07:06 +0000

    Initial commit

---
+++ src/main/java/com/kaznowski/versionedloadbalancer/App.java
@@ -0,0 +1,13 @@
+package com.kaznowski.versionedloadbalancer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
